a6399f1bb27d3d16d413a869cc5281d2f88d5f9f
gimquokka <gimquokka@gmail.com>
2022-10-29 16:33:23 +0900

    [Chore] change ping controller response

--- src/main/kotlin/me/ausg/api/helloawsapprunner/ping/PingController.kt
+++ src/main/kotlin/me/ausg/api/helloawsapprunner/ping/PingController.kt
@@ -1,13 +1,13 @@
 package me.ausg.api.helloawsapprunner.ping
 
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class PingController() {
     @GetMapping("/ping")
-    fun getPing(): String {
-        return "pong"
-    }
+    fun getPing(): ResponseEntity<String> =
+        ResponseEntity.ok("pong")
 }
 
